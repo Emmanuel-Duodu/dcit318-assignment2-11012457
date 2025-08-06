797aaff379dae639ab8bdebb390703446d80ec2f
Emmanuel-Duodu <osae20002@gmail.com>
2025-08-06 07:34:55 +0000

    commited program on Abstract Classes

---
+++ ShapesProgram/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+public abstract class Shape
+{
+    
+    public abstract double GetArea();
+}
+
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Abstract Classes and Methods ---");
+        
+
+        Circle myCircle = new Circle(5);
+        Rectangle myRectangle = new Rectangle(10, 4);
+
+
+        Console.WriteLine($"Area of the circle: {myCircle.GetArea():F2}");
+        Console.WriteLine($"Area of the rectangle: {myRectangle.GetArea()}");
+    }
+}
