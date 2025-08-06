cac3f8b163bceef916a2b75cbdd0d08589c484d8
Emmanuel-Duodu <osae20002@gmail.com>
2025-08-06 07:32:58 +0000

    Commited program on Inheritance

---
+++ AnimalsProgram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+public class Animal
+{
+    
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound.");
+    }
+}
+
+
+public class Dog : Animal
+{
+    
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark!");
+    }
+}
+
+// Derived class Cat
+public class Cat : Animal
+{
+   
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Inheritance and Method Overriding ---");
+        
+    
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+      
+        myAnimal.MakeSound(); 
+        myDog.MakeSound();   
+        myCat.MakeSound();    
+    }
+}
